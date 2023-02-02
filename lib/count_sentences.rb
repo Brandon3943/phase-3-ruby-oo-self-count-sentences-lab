13e5e9caaf4b23796d43746070635dfaf5390efe
root <root@Ty-Laptop.localdomain>
2023-02-02 15:10:44 -0500

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def report_on_self
+    "Self is: #{self}"
   end
 
-  def question?
+  def sentence?
+    self.end_with?(".")
+  end
 
+
+  def question?
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence = self.split(/\.|\?|!/)
+    sentence.filter { |sentence| !sentence.empty? }.size
   end
+  
 end
